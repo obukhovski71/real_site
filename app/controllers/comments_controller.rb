13c26dfbe10f93dada83ceb0c07704f725359b66
Andrei Abukhovski <obukhovski@yahoo.com>
2011-12-12 22:05:12 -0800

    3rd today's commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
-class PhotoCommentsController < ApplicationController
+class CommentsController < ApplicationController
+  def new
+    logger.debug("===================================")
+    logger.debug("Controller PhotoComments/Action New")
+    logger.debug("===================================")
+    logger.debug("object #{@user.inspect}")
+    logger.debug("===================================")
+    puts params.inspect
+    logger.debug("===================================")
+    @photo = Photo.find(params[:photo_id])
+    @comment = Comment.new(:photo_id => params[:photo_id])
+    
+  end
+
+  def create
+    logger.debug("===================================")
+    logger.debug("Controller PhotoComments/Action New")
+    logger.debug("===================================")
+    logger.debug("object #{@user.inspect}")
+    logger.debug("===================================")
+    puts params.inspect
+    logger.debug("===================================")
+    @comment = @photo.comments.create(params[:comment])
+    if @comment.save
+    redirect_to(:controller => "users", :action => "list_photos", :user_id => @photo.user_id)
+    else
+    render("new")
+    end
+  end
 end
